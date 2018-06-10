e8f99ddb8b2336fb5c604198350bb350bfdb12e3
BuildTools <unconfigured@null.spigotmc.org>
2018-06-10 18:53:18 +0200

    - added p slot tab - added skill slot tab

--- PQSE-GUI/PQSE-GUI/MainWindow.xaml.cs
+++ PQSE-GUI/PQSE-GUI/MainWindow.xaml.cs
@@ -146,6 +146,7 @@ namespace PQSE_GUI
 
         private void pokemonList_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
+            checkUnlockPSlots.IsChecked = false;
             int selectedPokeIndx = pokemonList.SelectedIndex;
             var allPoke = currentView.Save.SerializeData.characterStorage.characterDataDictionary;
 
@@ -154,12 +155,31 @@ namespace PQSE_GUI
             {
                 if(counter >= selectedPokeIndx)
                 {
+
+                    // general
                     txtPokeSpecies.Text = poke.Value.data.monsterNo.ToString();
                     txtPokeName.Text = TransformPokeName(poke.Value.data.name);
                     txtPokeLevel.Text = poke.Value.data.level.ToString();
                     txtPokeExp.Text = poke.Value.data.exp.ToString();
                     txtPokeHP.Text = poke.Value.data.hp.ToString();
                     txtPokeAttack.Text = poke.Value.data.attack.ToString();
+
+
+                    // pstones
+
+                    pStone0.SelectedIndex = poke.Value.data.potential.slotPropertyTypes[0];
+                    pStone1.SelectedIndex = poke.Value.data.potential.slotPropertyTypes[1];
+                    pStone2.SelectedIndex = poke.Value.data.potential.slotPropertyTypes[2];
+                    pStone3.SelectedIndex = poke.Value.data.potential.slotPropertyTypes[3];
+                    pStone4.SelectedIndex = poke.Value.data.potential.slotPropertyTypes[4];
+                    pStone5.SelectedIndex = poke.Value.data.potential.slotPropertyTypes[5];
+                    pStone6.SelectedIndex = poke.Value.data.potential.slotPropertyTypes[6];
+                    pStone7.SelectedIndex = poke.Value.data.potential.slotPropertyTypes[7];
+                    pStone8.SelectedIndex = poke.Value.data.potential.slotPropertyTypes[8];
+
+
+
+
 
                     string iconpath = "icons/pokemon/" + poke.Value.data.monsterNo.ToString() + ".png" ;
                     pokeIcon.Source = new BitmapImage(new Uri(iconpath, UriKind.Relative));
@@ -216,9 +236,7 @@ namespace PQSE_GUI
                         tmpVal = txtYellowUncommon.Text;
                         break;
                 }
-
                 currentView.Save.SerializeData.itemStorage.datas.Where(x => x.id == item.id).FirstOrDefault().num = (short)Convert.ToInt32(tmpVal);
-
             }                   
         }
 
@@ -249,6 +267,17 @@ namespace PQSE_GUI
                     currentView.Save.SerializeData.characterStorage.characterDataDictionary[counter].data.level = (ushort)Convert.ToInt32(txtPokeLevel.Text);
                     currentView.Save.SerializeData.characterStorage.characterDataDictionary[counter].data.exp = (uint)Convert.ToInt32(txtPokeExp.Text);
                     currentView.Save.SerializeData.characterStorage.characterDataDictionary[counter].data.hp = Convert.ToInt32(txtPokeHP.Text);
+
+                    currentView.Save.SerializeData.characterStorage.characterDataDictionary[counter].data.potential.slotPropertyTypes[0] = (sbyte)pStone0.SelectedIndex;
+                    currentView.Save.SerializeData.characterStorage.characterDataDictionary[counter].data.potential.slotPropertyTypes[1] = (sbyte)pStone1.SelectedIndex;
+                    currentView.Save.SerializeData.characterStorage.characterDataDictionary[counter].data.potential.slotPropertyTypes[2] = (sbyte)pStone2.SelectedIndex;
+                    currentView.Save.SerializeData.characterStorage.characterDataDictionary[counter].data.potential.slotPropertyTypes[3] = (sbyte)pStone3.SelectedIndex;
+                    currentView.Save.SerializeData.characterStorage.characterDataDictionary[counter].data.potential.slotPropertyTypes[4] = (sbyte)pStone4.SelectedIndex;
+                    currentView.Save.SerializeData.characterStorage.characterDataDictionary[counter].data.potential.slotPropertyTypes[5] = (sbyte)pStone5.SelectedIndex;
+                    currentView.Save.SerializeData.characterStorage.characterDataDictionary[counter].data.potential.slotPropertyTypes[6] = (sbyte)pStone6.SelectedIndex;
+                    currentView.Save.SerializeData.characterStorage.characterDataDictionary[counter].data.potential.slotPropertyTypes[7] = (sbyte)pStone7.SelectedIndex;
+                    currentView.Save.SerializeData.characterStorage.characterDataDictionary[counter].data.potential.slotPropertyTypes[8] = (sbyte)pStone8.SelectedIndex;
+
                     MessageBox.Show("Successfully wrote data!");
                     break;
                 }
@@ -256,6 +285,31 @@ namespace PQSE_GUI
             }
         }
 
-        
+        private void checkUnlockPSlots_Checked(object sender, RoutedEventArgs e)
+        {
+            if(checkUnlockPSlots.IsChecked == true)
+            {
+                int selectedPokeIndx = pokemonList.SelectedIndex;
+
+                var allPoke = currentView.Save.SerializeData.characterStorage.characterDataDictionary;
+
+                int counter = 0;
+                foreach (var poke in allPoke)
+                {
+                    if (counter >= selectedPokeIndx)
+                    {
+                        for (int i = 0; i < 9; i++)
+                        {
+                            currentView.Save.SerializeData.characterStorage.characterDataDictionary[counter].data.potential.slotPropertyTypes[i] = 2;
+                        }
+                        //currentView.Save.SerializeData.characterStorage.characterDataDictionary[counter].data.potential.nextSlotProgress = 5;
+                        currentView.Save.SerializeData.characterStorage.characterDataDictionary[counter].data.potential.nextActivateSlotIndex = 8;
+                        currentView.Save.SerializeData.characterStorage.characterDataDictionary[counter].data.potential.activeSlots = 255;
+                        break;
+                    }
+                    counter++;
+                }
+            }
+        }
     }
 }
