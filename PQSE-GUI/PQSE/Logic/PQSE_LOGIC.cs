5c2a3ca70996f5fccd032e31b9ffd03760d6ca82
xSillusx <tkattanek00@gmail.com>
2018-12-06 11:14:45 +0100

    Load and Export

--- PQSE-GUI/PQSE/Logic/PQSE_LOGIC.cs
+++ PQSE-GUI/PQSE/Logic/PQSE_LOGIC.cs
@@ -22,6 +22,29 @@ namespace PQSE
                 selectedFile = chooseSaveFileDialog.FileName;
             }
             CurrentView.LoadedSave = new SaveManager(System.IO.File.ReadAllBytes(selectedFile));
+
+            CurrentView.PokemonStorage = CurrentView.LoadedSave.SerializeData.characterStorage.characterDataDictionary;
+
         }
+
+        public void ExportSave()
+        {
+            if (CurrentView.LoadedSave != null)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    if (saveFileDialog.FileName != string.Empty)
+                    {
+                        System.IO.File.WriteAllBytes(saveFileDialog.FileName, CurrentView.LoadedSave.Export());
+                    }
+
+                }
+            }
+        }
+
+
+
+
     }
 }
--- PQSE-GUI/PQSE/Logic/ViewModel.cs
+++ PQSE-GUI/PQSE/Logic/ViewModel.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZeroFormatter;
 
 namespace PQSE
 {
@@ -23,6 +24,20 @@ namespace PQSE
             {
                 loadedSave = value;
                 OnPropertyChanged(nameof(ViewModel.LoadedSave));
+            }
+        }
+
+        private ILazyDictionary<int, CharacterStorage.ManageData> pokemonStorage = null;
+        public ILazyDictionary<int, CharacterStorage.ManageData> PokemonStorage
+        {
+            get
+            {
+                return pokemonStorage;
+            }
+            set
+            {
+                pokemonStorage = value;
+                OnPropertyChanged(nameof(ViewModel.PokemonStorage));
             }
         }
 
